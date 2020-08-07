41f76df6acf245cc7269c9e1ccf6ff57f7f30c90
oleoot <oleoot28@gmail.com>
2020-08-07 15:52:42 +0300

    Styles for user to see selected answer

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -4,11 +4,18 @@
       <template slot="lead">{{currentQuestion.question}}</template>
 
       <hr class="my-4" />
-
-      <p v-for="(answer, index) in answers" :key="index">{{answer}}</p>
-
-      <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#" @click="next">Next</b-button>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+        >{{answer}}</b-list-group-item>
+      </b-list-group>
+      <div class="btn-wrap">
+        <b-button variant="primary" href="#">Submit</b-button>
+        <b-button variant="success" href="#" @click="next">Next</b-button>
+      </div>
     </b-jumbotron>
   </div>
 </template>
@@ -27,5 +34,40 @@ export default {
       return answers;
     },
   },
+  data() {
+    return {
+      selectedIndex: null,
+    };
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    },
+  },
 };
 </script>
+
+<style>
+.btn {
+  margin: 0 5px;
+}
+.btn-wrap {
+  margin: 20px auto 0;
+  width: fit-content;
+}
+.list-group-item {
+  cursor: pointer;
+}
+/* .list-group-item:hover {
+  background: #eee;
+} */
+.selected {
+  background: lightblue;
+}
+.correct {
+  background: lightgreen;
+}
+.incorrect {
+  background: red;
+}
+</style>
